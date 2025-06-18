b29dc66efd5d4d1f8fb5d1eaa1fdb1820b793dd4
Sonu <sonu@Sonus-MacBook-Pro.local>
2025-06-18 17:20:14 +0545

    First Push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600">
+        ✅ Tailwind is working!
+      </h1>
+    </div>
+  );
+}
