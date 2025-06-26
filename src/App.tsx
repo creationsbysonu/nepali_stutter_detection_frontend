0760170afd3b0be3b868e3e7579fb26940501695
Sonu <sonu@Sonus-MacBook-Pro.local>
2025-06-26 17:05:32 +0545

    First Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.jsx or src/App.tsx
 
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./components/Dashboard";
 import StartTest from "./pages/StartTest";
